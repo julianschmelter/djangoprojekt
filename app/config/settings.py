8bc8a6968cabe4be6676224c6803f514a7132282
julianschmelter <jschmelt@stud.hs-bremen.de>
2026-06-16 10:56:58 +0200

    add REST API

--- app/config/settings.py
+++ app/config/settings.py
@@ -40,7 +40,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'testapp',
-]
+    'rest_framework',
+    'drf_spectacular',
+ ]
+ 
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'MeinXOEVStandard API',
+    'DESCRIPTION': 'API für MeinXOEVStandard',
+    'VERSION': '1.0.0',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- app/testapp/views.py
+++ app/testapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 from .models import MeinXOEVStandard
+from rest_framework import viewsets
+from .serializers import MeinXOEVStandardSerializer
 
 # Create your views here.
 def home(request): 
@@ -13,3 +15,7 @@ def meinXoevStandard_list(request):
 def meinXoevStandard_list_kern(request):
     daten = MeinXOEVStandard.objects.all().order_by("lieferant")
     return render(request, "meinxoevstandard_list_kern.html", {"daten": daten})
+
+class MeinXOEVStandardViewSet(viewsets.ModelViewSet):
+    queryset = MeinXOEVStandard.objects.all().order_by("lieferant")
+    serializer_class = MeinXOEVStandardSerializer
